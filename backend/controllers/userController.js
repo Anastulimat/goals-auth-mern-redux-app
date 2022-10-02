943d2fda5c0242b89c0f0a53dc834220e5c6ecf9
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2022-10-02 12:44:30 +0200

    feat: generate JWT for register and login methods

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,6 +44,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         });
     } else {
         res.status(400);
@@ -71,6 +72,7 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         });
     } else {
         res.status(400);
@@ -90,6 +92,12 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: "User data display" });
 });
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
+};
 
 module.exports = {
     registerUser,
