9bee9b8ce7706421b6e131174e61df1f02b49e73
TULIMAT Anas OBS/CSO <TZKM2460@orange.com>
2022-10-02 12:20:24 +0200

    feat: create user with hashed password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,9 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+
+const User = require("../models/userModel");
+
 /**
  * @route   POST /api/users
  *
@@ -7,9 +13,43 @@
  * @desc    Regestir a new user
  * @acces   public
  */
-const registerUser = (req, res) => {
-    res.json({ message: "Register User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please add all fields !");
+    }
+
+    // Check if user exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exsits");
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data");
+    }
+});
 
 /**
  * @route   POST /api/users/login
@@ -20,9 +60,9 @@ const registerUser = (req, res) => {
  * @desc    Authenticate a user
  * @acces   public
  */
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({ message: "Login User" });
-};
+});
 
 /**
  * @route   Get /api/users/me
@@ -33,9 +73,9 @@ const loginUser = (req, res) => {
  * @desc    Get user data
  * @acces   public
  */
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
     res.json({ message: "User data display" });
-};
+});
 
 module.exports = {
     registerUser,
