43b2b6ae2917be1cbaa4a945d1a2bbb589a998d3
Anas Tulimat <anastulimat@macbook-pro-de-anas.home>
2022-10-04 22:42:03 +0200

    feat: setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
 function App() {
     return (
-        <div>
-            <h1>Hello React</h1>
-        </div>
+        <>
+            <Router>
+                <div className="container"></div>
+                <Routes>
+                    <Route path="/" element={<Dashboard />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                </Routes>
+            </Router>
+        </>
     );
 }
 
