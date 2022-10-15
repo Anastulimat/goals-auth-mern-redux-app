f2bacba507550c40a361274ffd6f597934b7bf7a
Anas Tulimat <anastulimat@macbook-pro-de-anas.home>
2022-10-15 22:45:39 +0200

    feat: fetch goals after create goal

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,38 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import GoalFrom from "../components/GoalFrom";
+import { getGoals, reset } from "../features/goals/goalSlice";
+import Spinner from "../components/Spinner";
 
 function Dashboard() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const { user } = useSelector((state) => state.auth);
+    const { goals, isLoading, isError, message } = useSelector(
+        (state) => state.goals
+    );
 
     useEffect(() => {
+        if (isError) {
+            console.log(message);
+        }
         if (!user) {
             navigate("/login");
         }
-    }, [user, navigate]);
+
+        dispatch(getGoals());
+
+        return () => {
+            dispatch(reset());
+        };
+    }, []);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
 
     return (
         <>
